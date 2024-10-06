4ef1b91a0caef9a6f2e153e11b31af3fcabdb257
sonic19972009 <sonic19972009@yandex.ru>
2024-10-06 14:04:03 +0300

    2.Shop List

--- cookbook.py
+++ cookbook.py
@@ -14,3 +14,19 @@ with open('recipes.txt', 'rt', encoding='utf-8') as file:
             cook_book.get(food).append(dict(ingredient_name=a, quantity=int(b), measure=c))
 
 pprint(cook_book)
+
+def get_shop_list_by_foods(food_list, person_count):
+    shop_list = {}
+    for food in food_list:
+        if food in cook_book:
+            for ingredient in cook_book[food]:
+                if ingredient['ingredient_name'] in shop_list:
+                    shop_list[ingredient['ingredient_name']]['quantity'] += ingredient['quantity'] * person_count
+                else:
+                    shop_list[ingredient['ingredient_name']] = ({'measure': ingredient['measure'], 'quantity':
+                                                                (ingredient['quantity'] * person_count)})
+        else:
+            print('Такого блюда нет в книге')
+    return shop_list
+
+pprint(get_shop_list_by_foods(['Фахитос', 'Омлет'], 2))
