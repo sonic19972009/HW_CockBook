d267860a7f666f5377d7f58c88628f1a4e1f1a8b
sonic19972009 <sonic19972009@yandex.ru>
2024-10-06 22:13:10 +0300

    finalizing mergers

--- cookbook.py
+++ cookbook.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from glob import glob
 
 cook_book = {}
 with open('recipes.txt', 'rt', encoding='utf-8') as file:
@@ -15,6 +16,7 @@ with open('recipes.txt', 'rt', encoding='utf-8') as file:
 
 pprint(cook_book)
 
+
 def get_shop_list_by_foods(food_list, person_count):
     shop_list = {}
     for food in food_list:
@@ -24,46 +26,33 @@ def get_shop_list_by_foods(food_list, person_count):
                     shop_list[ingredient['ingredient_name']]['quantity'] += ingredient['quantity'] * person_count
                 else:
                     shop_list[ingredient['ingredient_name']] = ({'measure': ingredient['measure'], 'quantity':
-                                                                (ingredient['quantity'] * person_count)})
+                        (ingredient['quantity'] * person_count)})
         else:
             print('Такого блюда нет в книге')
     return shop_list
 
+
 pprint(get_shop_list_by_foods(['Фахитос', 'Омлет'], 2))
 
+file_names = []
 
-with open('1.txt', 'r', encoding='utf-8') as file_1:
-    line_1 = {}
-    count_1 = 0
-    for line in file_1.readlines():
-        count_1 += 1
-        line_1['1.txt'] = count_1
-with open('1.txt', 'r', encoding='utf-8') as file_1:
-    text_1 = file_1.read()
+for i in glob('task3\\*.txt'):
+    file_names.append(i)
 
-with open('2.txt', 'r', encoding='utf-8') as file_2:
-    line_2 = {}
-    count_2 = 0
-    for line in file_2.readlines():
-        count_2 += 1
-        line_2['2.txt'] = count_2
-with open('2.txt', 'r', encoding='utf-8') as file_2:
-    text_2 = file_2.read()
+file_contents = []
 
-with open('3.txt', 'r', encoding='utf-8') as file_3:
-    line_3 = {}
-    count_3 = 0
-    for line in file_3.readlines():
-        count_3 += 1
-        line_3['3.txt'] = count_3
-with open('3.txt', 'r', encoding='utf-8') as file_3:
-    text_3 = file_3.read()
+for filename in file_names:
+    with open(filename, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        file_contents.append((filename, len(lines), lines))
 
-join = sorted(list(line_1.items()) + list(line_2.items()) + list(line_3.items()), key=lambda x: x[1])
+file_contents.sort(key=lambda x: x[1])
 
-with open('result.txt', 'w', encoding='utf-8') as f_result:
-    for line in join:
-        f_result.write(f'{join[0][0]}\n {join[0][1]}\n {text_2}\n {join[1][0]}\n {join[1][1]}\n {text_1}\n'
-                          f'{join[2][0]}\n {join[2][1]}\n {text_3}\n')
-
-print(f_result)
+with open('result.txt', 'w', encoding='utf-8') as result_file:
+    for file_info in file_contents:
+        file_name, num_lines, lines = file_info
+        result_file.write(f"{file_name}\n")
+        result_file.write(f"{num_lines}\n")
+        for line in lines:
+            result_file.write(line)
+        result_file.write('\n')
