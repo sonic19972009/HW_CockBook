5bf2f893bda9bf2c87640a35b035605ac8c31563
sonic19972009 <sonic19972009@yandex.ru>
2024-10-06 14:15:39 +0300

    3.Merger

--- cookbook.py
+++ cookbook.py
@@ -30,3 +30,40 @@ def get_shop_list_by_foods(food_list, person_count):
     return shop_list
 
 pprint(get_shop_list_by_foods(['Фахитос', 'Омлет'], 2))
+
+
+with open('1.txt', 'r', encoding='utf-8') as file_1:
+    line_1 = {}
+    count_1 = 0
+    for line in file_1.readlines():
+        count_1 += 1
+        line_1['1.txt'] = count_1
+with open('1.txt', 'r', encoding='utf-8') as file_1:
+    text_1 = file_1.read()
+
+with open('2.txt', 'r', encoding='utf-8') as file_2:
+    line_2 = {}
+    count_2 = 0
+    for line in file_2.readlines():
+        count_2 += 1
+        line_2['2.txt'] = count_2
+with open('2.txt', 'r', encoding='utf-8') as file_2:
+    text_2 = file_2.read()
+
+with open('3.txt', 'r', encoding='utf-8') as file_3:
+    line_3 = {}
+    count_3 = 0
+    for line in file_3.readlines():
+        count_3 += 1
+        line_3['3.txt'] = count_3
+with open('3.txt', 'r', encoding='utf-8') as file_3:
+    text_3 = file_3.read()
+
+join = sorted(list(line_1.items()) + list(line_2.items()) + list(line_3.items()), key=lambda x: x[1])
+
+with open('result.txt', 'w', encoding='utf-8') as f_result:
+    for line in join:
+        f_result.write(f'{join[0][0]}\n {join[0][1]}\n {text_2}\n {join[1][0]}\n {join[1][1]}\n {text_1}\n'
+                          f'{join[2][0]}\n {join[2][1]}\n {text_3}\n')
+
+print(f_result)
